a91d834a3be38cb4dc8134f9ac8990f49cd4451b
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-12-10 14:08:35 +0000

    add transaction sample

---
+++ block_chain/main.py
@@ -0,0 +1,15 @@
+import datetime
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class Transaction:
+    time: datetime
+    sender: str
+    receiver: str
+    amount:int
+
+
+t1 = Transaction(datetime.datetime.now(), 'A', 'B', 1)
+t2 = Transaction(datetime.datetime.now(), 'C', 'D', 3)
+transactions = [t1,t2]
+print(transactions)
