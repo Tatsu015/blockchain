de458856d1324a4cfc7655e0179fe0ea055799b9
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-12-11 11:22:11 +0000

    add segnature

--- blockchain/main.py
+++ blockchain/main.py
@@ -1,4 +1,5 @@
 import datetime
+from ecdsa import SigningKey, SECP256k1, BadDigestError
 from dataclasses import dataclass
 
 
@@ -14,3 +15,19 @@ t1 = Transaction(datetime.datetime.now(), "A", "B", 1)
 t2 = Transaction(datetime.datetime.now(), "C", "D", 3)
 transactions = [t1, t2]
 print(transactions)
+
+secret_key = SigningKey.generate(curve=SECP256k1)
+print("secret key", secret_key.to_string().hex())
+public_key = secret_key.verifying_key
+print("public key", public_key.to_string().hex())
+
+doc = "very important data"
+signature = secret_key.sign(doc.encode("utf-8"))
+print("signature", signature.hex())
+
+# doc = "aaa"
+try:
+    public_key.verify(signature, doc.encode("utf-8"))
+    print("not falsificated")
+except BadDigestError:
+    print("falsificated")
