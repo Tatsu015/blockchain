d813d2f6028d748daed9fae2326b82167cf2230d
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-12-14 13:52:56 +0000

    add main fn

--- blockchain/main.py
+++ blockchain/main.py
@@ -51,32 +51,13 @@ def new_transaction(
     return t
 
 
-from_secret_key = "9a77f929737b0b2e90090afc57685d734735052deab172aa5228aa65ee0fcbd2"
-to_public_key = "b2ec566cff3702724e86ef6fa0d36835d6d5153ff402bca6dc976b7dc308f4bebeda361ae3267d0c3818ca001478f8ac8eb07908ed2e2c4b76cbcfd49720d4dd"
-t = new_transaction(from_secret_key, to_public_key, 1)
+def main():
+    from_secret_key = "9a77f929737b0b2e90090afc57685d734735052deab172aa5228aa65ee0fcbd2"
+    to_public_key = "b2ec566cff3702724e86ef6fa0d36835d6d5153ff402bca6dc976b7dc308f4bebeda361ae3267d0c3818ca001478f8ac8eb07908ed2e2c4b76cbcfd49720d4dd"
+    t = new_transaction(from_secret_key, to_public_key, 1)
 
-pd.to_pickle(t, "signed_transaction.pkl")
-
-# pd.to_pickle(t, "signed_transaction.pkl")
-
-# t1 = Transaction(datetime.datetime.now(), "A", "B", 1)
-# t2 = Transaction(datetime.datetime.now(), "C", "D", 3)
-# transactions = [t1, t2]
-# print(transactions)
-
-# secret_key = SigningKey.generate(curve=SECP256k1)
-# print("secret key", secret_key.to_string().hex())
-# public_key = secret_key.verifying_key
-# print("public key", public_key.to_string().hex())
-
-# doc = "very important data"
-# signature = secret_key.sign(doc.encode("utf-8"))
-# print("signature", signature.hex())
+    pd.to_pickle(t, "signed_transaction.pkl")
 
 
-# doc = "aaa"
-# try:
-#     public_key.verify(signature, doc.encode("utf-8"))
-#     print("not falsificated")
-# except BadSignatureError:
-#     print("falsificated")
+if __name__ == "__main__":
+    main()
