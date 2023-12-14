62d79cc8e0110346282a941cb10e17acc358e38b
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-12-14 13:51:17 +0000

    add verify failed test

--- blockchain/main.py
+++ blockchain/main.py
@@ -1,7 +1,7 @@
 import datetime
 import pandas as pd
 import json
-from ecdsa import SigningKey, SECP256k1, BadSignatureError
+from ecdsa import SigningKey, SECP256k1
 import binascii
 from dataclasses import dataclass, asdict
 
@@ -13,8 +13,9 @@ class UnsignedTransaction:
     receiver: str
     amount: int
 
-    def to_dict(self)->dict:
+    def to_dict(self) -> dict:
         return asdict(self)
+
 
 @dataclass(frozen=True)
 class Transaction:
@@ -24,20 +25,31 @@ class Transaction:
     amount: int
     signature: str
 
-    def to_unsigned(self)->UnsignedTransaction:
-        return UnsignedTransaction(self.time, self.sender,self.receiver,self.amount)
+    def to_unsigned(self) -> UnsignedTransaction:
+        return UnsignedTransaction(self.time, self.sender, self.receiver, self.amount)
 
-def new_transaction(from_secret_key:str, to_public_key:str, amount:int)->Transaction:
-    from_sec_key = SigningKey.from_string(binascii.unhexlify(from_secret_key), curve=SECP256k1)
+
+def new_transaction(
+    from_secret_key: str, to_public_key: str, amount: int
+) -> Transaction:
+    from_sec_key = SigningKey.from_string(
+        binascii.unhexlify(from_secret_key), curve=SECP256k1
+    )
     from_pub_key = from_sec_key.verifying_key.to_string().hex()
 
     now = datetime.datetime.now(datetime.timezone.utc).isoformat()
-    unsigned = {"time":now, "sender":from_pub_key, "receiver":to_public_key, "amount":amount}
-   
-    s = from_sec_key.sign(json.dumps(unsigned).encode('utf-8')).hex()
+    unsigned = {
+        "time": now,
+        "sender": from_pub_key,
+        "receiver": to_public_key,
+        "amount": amount,
+    }
+
+    s = from_sec_key.sign(json.dumps(unsigned).encode("utf-8")).hex()
 
     t = Transaction(now, from_pub_key, to_public_key, amount, s)
     return t
+
 
 from_secret_key = "9a77f929737b0b2e90090afc57685d734735052deab172aa5228aa65ee0fcbd2"
 to_public_key = "b2ec566cff3702724e86ef6fa0d36835d6d5153ff402bca6dc976b7dc308f4bebeda361ae3267d0c3818ca001478f8ac8eb07908ed2e2c4b76cbcfd49720d4dd"
--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -3,21 +3,53 @@ import pandas as pd
 from ecdsa import SigningKey, SECP256k1, BadSignatureError, VerifyingKey
 import binascii
 import json
+import os
 from blockchain.main import Transaction, new_transaction
 
 PKL_FILE_PATH = "test_signed_transaction.pkl"
+
 
 @pytest.fixture
 def setup_transaction():
     from_secret_key = "9a77f929737b0b2e90090afc57685d734735052deab172aa5228aa65ee0fcbd2"
     to_public_key = "b2ec566cff3702724e86ef6fa0d36835d6d5153ff402bca6dc976b7dc308f4bebeda361ae3267d0c3818ca001478f8ac8eb07908ed2e2c4b76cbcfd49720d4dd"
     t = new_transaction(from_secret_key, to_public_key, 1)
-    pd.to_pickle(t,PKL_FILE_PATH)
+    pd.to_pickle(t, PKL_FILE_PATH)
+
+    yield
+
+    os.remove(PKL_FILE_PATH)
 
 
-def test_verify_transactin(setup_transaction):
-    transaction:Transaction = pd.read_pickle(PKL_FILE_PATH)
-    from_pub_key = VerifyingKey.from_string(binascii.unhexlify(transaction.sender), curve=SECP256k1)
-    signature = binascii.unhexlify(transaction.signature)
-    unsigned = transaction.to_unsigned().to_dict()
-    from_pub_key.verify(signature, json.dumps(unsigned).encode('utf-8'))
+def test_normal_transaction(setup_transaction):
+    sat: Transaction = pd.read_pickle(PKL_FILE_PATH)
+    from_pub_key = VerifyingKey.from_string(
+        binascii.unhexlify(sat.sender), curve=SECP256k1
+    )
+    signature = binascii.unhexlify(sat.signature)
+    unsigned = sat.to_unsigned().to_dict()
+
+    from_pub_key.verify(signature, json.dumps(unsigned).encode("utf-8"))
+
+    assert True is True
+
+
+def test_falsification_transaction(setup_transaction):
+    transaction: Transaction = pd.read_pickle(PKL_FILE_PATH)
+    sat = Transaction(
+        transaction.time,
+        transaction.sender,
+        transaction.receiver,
+        30,
+        transaction.signature,
+    )
+    from_pub_key = VerifyingKey.from_string(
+        binascii.unhexlify(sat.sender), curve=SECP256k1
+    )
+    signature = binascii.unhexlify(sat.signature)
+    unsigned = sat.to_unsigned().to_dict()
+
+    with pytest.raises(BadSignatureError) as e:
+        from_pub_key.verify(signature, json.dumps(unsigned).encode("utf-8"))
+
+    assert str(e.value) == "Signature verification failed"
