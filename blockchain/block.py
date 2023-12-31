13d7aa9417879305e9a5f121d0926614a14ed901
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-12-31 23:50:46 +0900

    add untimed block

--- blockchain/block.py
+++ blockchain/block.py
@@ -1,6 +1,17 @@
 from datetime import datetime
 from pydantic import BaseModel
+import hashlib
 from blockchain.transaction import Transaction
+
+
+class UntimedBlock(BaseModel):
+    transactions: list[Transaction]
+    hash: str
+    nonce: int
+
+    def hash(self) -> str:
+        h = hashlib.sha256(self.model_dump_json().encode("utf-8")).hexdigest()
+        return h
 
 
 class Block(BaseModel):
@@ -8,3 +19,12 @@ class Block(BaseModel):
     transactions: list[Transaction]
     hash: str
     nonce: int
+
+    def hash(self) -> str:
+        h = hashlib.sha256(self.model_dump_json().encode("utf-8")).hexdigest()
+        return h
+
+    def to_untimed(self) -> UntimedBlock:
+        return UntimedBlock(
+            transactions=self.transactions, hash=self.hash, nonce=self.nonce
+        )
