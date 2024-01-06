4baaaefcca91623cef4205afafc6b077db4ce5df
Tatsu015 <tatsumi.soichiro@gmail.com>
2024-01-06 16:39:54 +0900

    fix unused mining code

--- mining.py
+++ mining.py
@@ -9,7 +9,6 @@ from blockchain.block import Block
 from blockchain.blockchain import BlockChain
 from blockchain.transaction import Transaction
 
-# "51100f732f670c6223d4e341df8e5ab43a32da1c544d610493ca14b49de45488"
 miner = "95029604df9f99e55392a443ea5dff802c41c9548bc182665c2f6b870cc18d0de4a9e438125aab1e63970d2eb4f3859323a6bb0e340fad03cb8b9dac4f52a3b0"
 ip_addr = "127.0.0.1"
 blockchain = BlockChain()
@@ -19,7 +18,6 @@ if res_chain.status_code != 200:
     print(f"request error: {res_chain.status}")
     sys.exit()
 chain_json = res_chain.json()
-print(res_chain.text)
 blockchain.chain = TypeAdapter(list[Block]).validate_python(chain_json)
 
 res_trans = requests.get("http://" + ip_addr + ":8080/transaction_pool")
