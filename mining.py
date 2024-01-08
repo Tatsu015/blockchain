cc762a177332fcf58e19e87a0b99344dff6d2d44
Tatsu015 <tatsumi.soichiro@gmail.com>
2024-01-08 23:55:17 +0900

    fix empty list parse error

--- mining.py
+++ mining.py
@@ -1,17 +1,16 @@
 import requests
 import sys
 
-# import json
+import json
 from datetime import datetime
 
-# from pydantic import TypeAdapter
-# from pydantic.json import pydantic_encoder
-# from blockchain.block import Block
+from pydantic import TypeAdapter
+from pydantic.json import pydantic_encoder
+from blockchain.block import Block
 
 from blockchain.blockchain import BlockChain
-from blockchain.dto import BlocksDTO, TransactionsDTO
 
-# from blockchain.transaction import Transaction
+from blockchain.transaction import Transaction
 
 miner = "95029604df9f99e55392a443ea5dff802c41c9548bc182665c2f6b870cc18d0de4a9e438125aab1e63970d2eb4f3859323a6bb0e340fad03cb8b9dac4f52a3b0"
 ip_addr = "127.0.0.1"
@@ -22,7 +21,7 @@ if res_chain.status_code != 200:
     print(f"request error: {res_chain.status}")
     sys.exit()
 chain_json = res_chain.json()
-blockchain.chain = BlocksDTO.model_validate_json(json_data=chain_json)
+blockchain.chain = TypeAdapter(list[Block]).validate_python(chain_json)
 
 res_trans = requests.get("http://" + ip_addr + ":8080/transaction_pool")
 if res_trans.status_code != 200:
@@ -32,17 +31,13 @@ trans_json = res_trans.json()
 if trans_json == []:
     blockchain.transactions = []
 else:
-    blockchain.transactions = TransactionsDTO.validate_json(
-        trans_json
-    )  # TypeAdapter(list[Transaction]).validate_json(trans_json)
-
+    blockchain.transactions = TypeAdapter(list[Transaction]).validate_json(trans_json)
+print("++++++++++++++++++++++")
+print(blockchain)
 block = blockchain.find_new_block(now=datetime.now(), miner=miner)
-print(block)
 blockchain.chain.append(block)
 
-data = BlocksDTO(
-    __root__=blockchain.chain
-).model_dump_json()  # json.dumps(blockchain.chain, default=pydantic_encoder)
+data = json.dumps(blockchain.chain, default=pydantic_encoder)
 print("success mining!")
 print("chain:", data)
 res = requests.post("http://" + ip_addr + ":8080/chain", data=data)
