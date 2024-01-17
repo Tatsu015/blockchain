3fb38027672953c8b08149c2ad3c17f9c3db7c61
Tatsu015 <tatsumi.soichiro@gmail.com>
2024-01-17 23:58:51 +0900

    refactor mining method place

--- blockchain/usecase.py
+++ blockchain/usecase.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from blockchain.block import Block
-from blockchain.blockchain import Blockchain
+from blockchain.blockchain import Blockchain, find_new_block, has_minus_amount
 from blockchain.transaction import Transaction
 
 
@@ -36,3 +37,26 @@ class Usecase:
         except Exception as e:
             print(e)
             return str(e)
+
+    def mining(self, miner_public_key, transactions, chain):
+        blockchain = Blockchain()
+        copied_transactions = transactions.copy()
+        blockchain.chain = chain
+        blockchain.refresh_inblock_transactions()
+        copied_all_block_transactions = blockchain.inblock_transactions.copy()
+        for t in copied_transactions:
+            copied_all_block_transactions.append(t)
+            if has_minus_amount(copied_all_block_transactions):
+                transactions.remove(t)
+                copied_all_block_transactions.remove(t)
+
+        blockchain.outblock_transactions = transactions
+
+        block = find_new_block(
+            now=datetime.now(),
+            miner=miner_public_key,
+            outblock_transactions=blockchain.outblock_transactions,
+            chain=blockchain.chain,
+        )
+        blockchain.chain.append(block)
+        return blockchain
