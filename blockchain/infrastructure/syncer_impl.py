058552eb8b6d2fef70d38b52f11a386317b12aea
Tatsu015 <tatsumi.soichiro@gmail.com>
2024-01-30 20:31:50 +0900

    sync transaction in parallel

--- blockchain/infrastructure/syncer_impl.py
+++ blockchain/infrastructure/syncer_impl.py
@@ -9,17 +9,17 @@ from blockchain.settings import settings
 class SyncerImpl(Syncer):
     def bloadcast(self, transaction: Transaction):
         hosts = list(filter(None, settings.hosts.split(",")))
-        for host in hosts:
-            if host == settings.my_adress():
-                continue
-            url = "http://" + host + "/transaction/bloadcast"
-            data = jsonable_encoder(transaction)
-            requests.post(url, json=data)
+        # for host in hosts:
+        #     if host == settings.my_adress():
+        #         continue
+        #     url = "http://" + host + "/transaction/bloadcast"
+        #     data = jsonable_encoder(transaction)
+        #     requests.post(url, json=data)
 
-        # with ThreadPoolExecutor() as exector:
-        #     for host in hosts:
-        #         if host == settings.my_adress():
-        #             continue
-        #         url = "http://" + host + "/transaction/bloadcast"
-        #         json = jsonable_encoder(transaction)
-        #         exector.submit(requests.post, url, json)
+        with ThreadPoolExecutor() as exector:
+            for host in hosts:
+                if host == settings.my_adress():
+                    continue
+                url = "http://" + host + "/transaction/bloadcast"
+                json = jsonable_encoder(transaction)
+                exector.submit(requests.post, url, None, json)
